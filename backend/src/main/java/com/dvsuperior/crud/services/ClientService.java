424112bb1c0b2c43430c7adc6257640020b857da
Cauê Ferreira <kau3hc@hotmail.com>
2021-05-21 12:58:32 -0300

    Find By id

---
+++ backend/src/main/java/com/dvsuperior/crud/services/ClientService.java
@@ -0,0 +1,35 @@
+package com.dvsuperior.crud.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dvsuperior.crud.dto.ClientDTO;
+import com.dvsuperior.crud.entities.Client;
+import com.dvsuperior.crud.repositories.ClientRepository;
+import com.dvsuperior.crud.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,79 @@
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Maria Silva',
+        '92758799820',
+        5000.00,
+        '1990-07-21T10:30:00Z',
+        2
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Luana Mota',
+        '69969534807',
+        2000.00,
+        '1994-08-10T10:30:00Z',
+        1
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Gabriela Golçalves Da Silva',
+        '00185210880',
+        7000.00,
+        '1984-01-01T10:30:00Z',
+        3
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Mario Pereira silva',
+        '51029048843',
+        1500.00,
+        '2000-03-22T10:30:00Z',
+        0
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Fernando Sousa Martins',
+        '24277281877',
+        4500.00,
+        '1996-02-20T10:30:00Z',
+        1
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Camila Mendes Gomes',
+        '36186005852',
+        8000.00,
+        '1993-09-30T10:30:00Z',
+        2
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'João Lopes Leite',
+        '76264366870',
+        2350.00,
+        '1978-05-20T10:30:00Z',
+        0
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Gustavo Almeida Silva',
+        '62754304860',
+        2500.00,
+        '2001-04-11T10:30:00Z',
+        3
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Priscila Carvalho',
+        '79860612862',
+        10000.00,
+        '1991-07-04T10:30:00Z', 1
+    );
+INSERT INTO TB_CLIENT(name, cpf, income, birth_date, children)
+VALUES(
+        'Ana Paula Dantas',
+        '92911056868',
+        15000.00,
+        '1991-05-26T10:30:00Z',
+        0
+    );
